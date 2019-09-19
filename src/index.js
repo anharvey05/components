cacee15f1231035c944a4b86c2e7952df9813709
anharvey05 <amy.harvey@onshoreoutsourcing.com>
2019-09-19 14:05:12 -0500

    Added ApprovalCard.js, reworked components, and worked through part 38 of tutorial

--- src/index.js
+++ src/index.js
@@ -1,38 +1,31 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
+import CommentDetail from './CommentDetail';
+import ApprovalCard from './ApprovalCard';
 
 const App = () => {
     return (
         <div className="ui container comments">
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()}/>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Sam
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:00PM</span>
-                    </div>
-                    <div className="text">Nice blog post!</div>
-                </div>
-            </div>
-            <div className="comment">
-                <a href="/" className="avatar">
-                    <img alt="avatar" src={faker.image.avatar()}/>
-                </a>
-                <div className="content">
-                    <a href="/" className="author">
-                        Bob
-                    </a>
-                    <div className="metadata">
-                        <span className="date">Today at 6:48PM</span>
-                    </div>
-                    <div className="text">You did good work on this one.</div>
-                </div>
-            </div>
+            <ApprovalCard />
+            <CommentDetail 
+                author="Sam" 
+                timeAgo="Today at 4:45PM" 
+                content="Nice blog post!" 
+                avatar={faker.image.avatar()} 
+            />
+            <CommentDetail 
+                author="Alex" 
+                timeAgo="Today at 2:00AM" 
+                content="Where can I find the recipe?" 
+                avatar={faker.image.avatar()}
+            />
+            <CommentDetail 
+                author="Jane" 
+                timeAgo="Yesterday at 5:01PM" 
+                content="You're amazing!  Thank you!" 
+                avatar={faker.image.avatar()}
+            />
         </div>
     );
 };
